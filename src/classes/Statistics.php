5b14916800fc844d37f56494814dcb6b1a40f66b
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-01 18:25:59 +0700

    Update liputan6

--- src/classes/Statistics.php
+++ src/classes/Statistics.php
@@ -1,14 +1,27 @@
 <?php
 
 
-
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @package Phx\Scrapers
+ * @license MIT
+ * @version 0.0.1
+ */
 class Statistics
 {
+	/**
+	 * Constructor.
+	 *
+	 * @return void
+	 */
 	public function __construct()
 	{
 		$this->pdo = DB::pdo();
 	}
 
+	/**
+	 * @return void
+	 */
 	public function regional()
 	{
 		$st = $this->pdo->prepare(

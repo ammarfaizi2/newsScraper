07e30112a2d8b9b41b3c4f2940295bdfd9ac2d1c
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-02 23:52:38 +0700

    Fix undefined function

--- public/api.php
+++ public/api.php
@@ -4,4 +4,42 @@ require __DIR__."/../config/main.php";
 require __DIR__."/../config/scraper.php";
 require __DIR__."/../bootstrap/icetea_bootstrap.php";
 
+function tq($str, $max = 32)
+{
+	if (strlen($str) > $max) {
+		return substr($str, 0, $max)."...";
+	}
+	return $str;
+}
+
+function es($str)
+{
+	return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+}
+
+function gptz($n)
+{
+	switch ($n) {
+		case 1:
+			return "=";
+		case 2:
+			return "!=";
+		case 3:
+			return ">";
+		case 4:
+			return "<";
+		case 5:
+			return ">=";
+		case 6:
+			return "<=";
+		case 7:
+		case 8:
+		case 9:
+		case 10;
+			return "LIKE";
+		default:
+			return "=";
+	}
+}
+
 (new Api)->run();

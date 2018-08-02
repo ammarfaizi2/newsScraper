63dcc217d2c34d8fcd24f592109e832cfa0b5f0a
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-02 23:00:32 +0700

    Create check.php

--- public/index.php
+++ public/index.php
@@ -52,7 +52,7 @@ $st = $st->fetch(PDO::FETCH_NUM)[0];
 			<a href="statistics.php"><button>Advanced Statistics</button></a>
 			<a href="check.php"><button>Check Scraper Procceses</button></a>
 			<h3>This page was loaded at: <?php print date("Y-m-d H:i:s") ?> GMT+7</h3>
-			<h3>Latest Update from Scraper: <?php print $st; ?></h3>
+			<h3>Latest Update from Scraper: <?php print $st; ?>GMT+7</h3>
 		</div>
 		<div class="dd">
 			<h2>Regional Statistic</h2>

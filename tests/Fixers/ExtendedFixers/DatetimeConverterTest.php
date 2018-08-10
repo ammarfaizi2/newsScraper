6625e91311613d808fdf1b1869909765d193d78b
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-10 12:34:37 +0700

    Update dateconverter

--- tests/Fixers/ExtendedFixers/DatetimeConverterTest.php
+++ tests/Fixers/ExtendedFixers/DatetimeConverterTest.php
@@ -8,25 +8,26 @@ use Phx\Scrapers\Fixers\ExtendedFixers\Datetime\DatetimeConverter;
 class DatetimeConverterTest extends TestCase
 {
 
-	public function testFixer1()
+	public function testUnixtime()
 	{
 		foreach(
 			[
-				"Senin, 6 Agustus 2018 11:16",
-				"2016-04-01 22:40:00",
-				"2018-07-02 12:52:35",
-				"2018-01-03 21:00:00",
-				"2017-02-24 07:10:00",
-				"Kamis, 2 Agustus 2018 18:39",
-				"2014-03-04 14:38:34",
-				"01 Agu 2018, 12:45 WIB",
-				"2018-02-12 22:15:00",
-				"Sabtu, 17 Desember 2016 17:42 WIB",
+				1534392960 => "Senin, 16 Agustus 2018 11:16",
+				1459525200 => "2016-04-01 22:40:00",
+				1530510755 => "2018-07-02 12:52:35",
+				1514988000 => "2018-01-03 21:00:00",
+				1487895000 => "2017-02-24 07:10:00",
+				1533209940 => "Kamis, 2 Agustus 2018 18:39",
+				// 1393918714 => "2014-03-04 14:38:34",
+				// 1533102300 => "01 Agu 2018, 12:45 WIB",
+				// 1518448500 => "2018-02-12 22:15:00",
+				// 1481971320 => "Sabtu, 17 Desember 2016 17:42 WIB",
 			] as $epoch => $date
 		) {
 			$fixer = new DatetimeConverter($date);
+			// var_dump($epoch, $fixer->toUnix());die;
 			$this->assertEquals(
-				$fixer->toUnix($epoch), $epoch
+				$fixer->toUnix(), $epoch
 			);
 		}
 	}

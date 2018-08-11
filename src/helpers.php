87039662ed662a12a795bf6ea3882581785aee89
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-11 21:14:26 +0700

    Update helper and fix undefined variable

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,11 @@
 <?php
 
 if (! function_exists("icelog")) {
-	function icelog($logMsg)
+	function icelog($logMsg, ...$params)
 	{
-		fprintf(STDOUT, $logMsg = sprintf("[".date("d F Y h:i:s A")."] %s\n", $logMsg));
+		fprintf(STDOUT, $logMsg = sprintf("[".date("d F Y h:i:s A")."] %s\n", 
+			sprintf($logMsg, ...$params)
+		));
 		if (defined("LOG_FILE")) {
 			file_put_contents(LOG_DIR."/".LOG_FILE, $logMsg, FILE_APPEND | LOCK_EX);
 		}

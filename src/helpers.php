7a8bc1a549323074efaee5d6d1bff9235e0ae1b5
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-16 22:08:37 +0700

    Update suara pagination handler

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,11 @@
 <?php
 
 if (! function_exists("icelog")) {
-	function icelog($logMsg, ...$params)
+	/**
+	 * @param string $logMsg
+	 * @param mixed	 ...$params
+	 */
+	function icelog(string $logMsg, ...$params)
 	{
 		fprintf(STDOUT, $logMsg = sprintf("[".date("d F Y h:i:s A")."] %s\n", 
 			sprintf($logMsg, ...$params)
@@ -9,5 +13,16 @@ if (! function_exists("icelog")) {
 		if (defined("LOG_FILE")) {
 			file_put_contents(LOG_DIR."/".LOG_FILE, $logMsg, FILE_APPEND | LOCK_EX);
 		}
+	}
+}
+
+if (! function_exists("reg")) {
+	/**
+	 * @param array		$&a
+	 * @param \Closure	$c
+	 * @return void
+	 */
+	function reg(array &$a, Closure $c) {
+		$a[] = $c;
 	}
 }

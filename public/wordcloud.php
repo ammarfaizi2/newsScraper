3d43475d6a689937c07f3e062bc97cc2f4945aa1
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-19 19:59:51 +0700

    Update wordcloud

--- public/wordcloud.php
+++ public/wordcloud.php
@@ -24,7 +24,7 @@ if (isset($_GET["limit"])) {
 
 	$limit = abs((int)$_GET["limit"]);
 } else {
-	$limit = 10;
+	$limit = 15;
 }
 
 if (isset($_GET["n"])) {
@@ -66,39 +66,55 @@ $wc = $pdo->prepare("SELECT `a`.`count`,`a`.`words` FROM `title_wordcloud_region
 <head>
 	<title></title>
 	<style type="text/css">
-		.x {
+		* {
+			font-family: Arial;
+		}
+		.cage {
+			border: 1px solid #000;
+			width: 400px;
+			height: 500px;
+			overflow-y: scroll;
+			overflow-x: scroll;
 			display: inline-block;
-			margin-left: 3px;
-			margin-right: 3px;
+		}
+		table {
+			border-collapse: collapse;
+			width: 360px;
+		}
+		.pgg {
+			font-size: 20px;
+		}
+		.rqq {
+			color: blue;
+			cursor: pointer;
 		}
 	</style>
 </head>
 <body>
 	<center>
-		<?php $jj = 1;
-			while ($r = $st->fetch(PDO::FETCH_NUM)) {
-				$wc->execute([":n" => $n,":regional" => $r[0]]);
-if (!($jj % 3)) {
-	?><div><?php
-}
-?><div class="x">
-	<div><h3><?php print $r[0]; ?></h3></div>
-	<div><h4>(regional code: <?php print $r[1]; ?>)</h4></div>
-	<table border="1" style="border-collapse: collapse;">
-		<tr><td align="center">No.</td><td align="center">Words</td><td align="center">Amount</td></tr>
-<?php $i = 1;
-				while ($rr = $wc->fetch(PDO::FETCH_ASSOC)) {
-?><tr><td align="center"><?php print $i++; ?>.</td><td align="center"><?php print htmlspecialchars($rr["words"]); ?></td><td align="center"><?php print $rr["count"] ?></td></tr>
-<?php
-				}
-?>	</table>
-</div><?php
-if (!($jj % 3)) {
-	?></div><?php
-}$jj++;
-
-			}
-		?>
+	<h2>Top <?php print $n !== 1 ? $n : ""; $ii = 1; ?> Word<?php print $n === 1 ? "" : "s"; ?> Frequency</h2>
+	<h4 class="pgg">Change n value: &nbsp;<?php for ($i=1; $i <= 4; $i++) { 
+		?><a class="rqq" href="?n=<?php print $i; ?>"><button class="rqq" <?php print $i === $n ? "disabled":"";?>><span class="pgg"><?php print $i ?></span></button></a>&nbsp;&nbsp;&nbsp;<?php
+	} ?></h4>
+	<?php $jj = 0; while ($r = $st->fetch(PDO::FETCH_NUM)): $wc->execute([":n" => $n, ":regional" => $r[1]]); ?>
+		<div class="cage">
+			<p><?php print $ii++; ?></p>
+			<div>
+				<h3><?php print $r[0]; ?></h3>
+				<p>Regional Code: <?php print $r[1]; ?></p>
+			</div>
+			<table border="1">
+				<thead>
+						<tr><th align="center">No.</th><th align="center">Words</th><th align="center">Amount</th></tr>
+				</thead>
+				<tbody>
+					<?php $i = 1; while ($rr = $wc->fetch(PDO::FETCH_ASSOC)): ?>
+						<tr><td align="center"><?php print $i++; ?></td><td><?php print htmlspecialchars($rr["words"], ENT_QUOTES, "UTF-8"); ?></td><td><?php print $r["count"]; ?></td></tr>
+					<?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+	<?php endwhile; ?>
 	</center>
 </body>
 </html><?php unset($pdo); ?>

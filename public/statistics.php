bfd71b7414a9bbfeae486b6901b800d20cfa0153
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-22 15:06:04 +0700

    Update statistic page

--- public/statistics.php
+++ public/statistics.php
@@ -6,7 +6,13 @@
 <body>
 	<center>
 		<a href="index.php"><button style="cursor: pointer;">Back to Home</button></a>
-		<h1>This page is under construction</h1>
+		<div style="margin-top: 40px;">
+			<div>
+				<a href="wordcloud.php"><button>Wordcloud</button></a>
+				<a href="wordcloud_js.php"><button>Wordcloud JS</button></a>
+				<a href="wordcloud_api.php"><button>Wordcloud API</button></a>
+			</div>
+		</div>
 	</center>
 </body>
 </html>

0760e12074b2d6e1267252d3783963a9b2e3a39e
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-04 12:22:02 +0700

    Update scraper.php

--- bin/scraper.php
+++ bin/scraper.php
@@ -133,7 +133,12 @@ switch ($argv[1]) {
 	case 'gonews':
 		$st = new Gonews;
 		#define("LOG_FILE", "gonews.log");
-		break;		
+		break;
+
+	case 'banteninfo':
+		$st = new Banteninfo;
+		#define("LOG_FILE", "bateninfo.log");
+		break;
 
 
 	default:

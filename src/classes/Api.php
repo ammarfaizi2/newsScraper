7aa785cd318c1c07e9c368680a33e0bf33ff4fc0
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-05 18:37:55 +0700

    Fix API encoding

--- src/classes/Api.php
+++ src/classes/Api.php
@@ -197,6 +197,13 @@ final class Api
 			
 			$i++;
 		}
+		foreach($result as &$rr) {
+			foreach($rr as &$rrx) {
+				if (is_string($rrx)) {
+					$rrx = mb_convert_encoding($rrx, "UTF-8", "UTF-8");
+				}
+			}
+		}
 
 		print json_encode(
 			[
@@ -223,3 +230,20 @@ final class Api
 		exit();
 	}
 }
+
+
+if (!function_exists('json_last_error_msg')) {
+        function json_last_error_msg() {
+            static $ERRORS = array(
+                JSON_ERROR_NONE => 'No error',
+                JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+                JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
+                JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+                JSON_ERROR_SYNTAX => 'Syntax error',
+                JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+            );
+
+            $error = json_last_error();
+            return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
+        }
+    }

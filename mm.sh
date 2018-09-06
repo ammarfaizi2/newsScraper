025993dfc0a7a12ae6dae5ffe12c24172693d378
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-06 12:47:58 +0700

    Update mm.sh

--- mm.sh
+++ mm.sh
@@ -2,7 +2,7 @@
 
 merge='git merge master'
 checkout='git checkout'
-branches={datetime_parser,wordcloud,api,dev_{antaranews,detik,kompas,liputan6,tribunnews,suara,viva,kabardaerah,jpnn,indonesiatimur,sindonews,gonews}}
+branches={wordcloud,api,datetime_parser,dev_{ajnn,antaranews,banteninfo,detik,goaceh,gonews,indonesiatimur,jpnn,kabardaerah,kabarmedan,kabarsumut,kompas,liputan6,medansatu,modusaceh,portalsatu,prohaba,sindonews,suara,sumutpos,tribunnews,viva}}
 
 # Merge master branch to other branches.
 
